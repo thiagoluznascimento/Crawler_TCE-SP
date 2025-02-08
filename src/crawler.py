d9fbe3e8fe21fbcecb6584109a28feaf3ebcd86e
Thiago <thiagoifnmgsi2015@gmail.com>
2025-02-08 14:19:52 -0300

    Cria metodo que _obtem_pag_jurisprudencia.
    - Utilizando modulo sys para o user interagir com o terminal

--- src/crawler.py
+++ src/crawler.py
@@ -3,14 +3,23 @@ from bs4 import BeautifulSoup
 
 class BuscadorTceSp:
 
-    URL_BUSCA = "https://www.tce.sp.gov.br/jurisprudencia/"
+    URL_BASE = "https://www.tce.sp.gov.br/jurisprudencia/pesquisar"
+
+    def __init__(self, busca):
+        self.nome_busca = busca
+        print(f'Buscando por {self.nome_busca}')
+        # import pdb; pdb.set_trace()
 
     def baixa_docs(self):
-        self._obtem_pag_jurisprudencia(self.URL_BUSCA)
+        url_busca = f'{self.URL_BASE}?txtTdPalvs={self.nome_busca.replace(' ', '+')}&txtExp=&txtQqUma=&txtNenhPalvs=&txtNumIni=&txtNumFim=&tipoBuscaTxt=Documento&_tipoBuscaTxt=on&quantTrechos=1&processo=&exercicio=&dataAutuacaoInicio=&dataAutuacaoFim=&dataPubInicio=&dataPubFim=&_relator=1&_auditor=1&_materia=1&_tipoDocumento=1&acao=Executa'
+        self._obtem_pag_jurisprudencia(url_busca)
 
-    def _obtem_pag_jurisprudencia( self, URL_BUSCA):
+
+    
+
+    def _obtem_pag_jurisprudencia( self, url_busca):
         try:
-            r = requests.get(self.URL_BUSCA, auth=('user', 'pass'))
+            r = requests.get(url_busca)
             resposta = r.status_code
             print(r.text)
             # import pdb; pdb.set_trace();
