7077e048e6f87cf030267208e965f2a0f88e1b3b
Thiago <thiagoifnmgsi2015@gmail.com>
2025-02-09 12:38:30 -0300

    Cria metodo extrai tabela

--- src/crawler.py
+++ src/crawler.py
@@ -12,17 +12,22 @@ class BuscadorTceSp:
 
     def baixa_docs(self):
         url_busca = f'{self.URL_BASE}?txtTdPalvs={self.nome_busca.replace(' ', '+')}&txtExp=&txtQqUma=&txtNenhPalvs=&txtNumIni=&txtNumFim=&tipoBuscaTxt=Documento&_tipoBuscaTxt=on&quantTrechos=1&processo=&exercicio=&dataAutuacaoInicio=&dataAutuacaoFim=&dataPubInicio=&dataPubFim=&_relator=1&_auditor=1&_materia=1&_tipoDocumento=1&acao=Executa'
-        self._obtem_pag_jurisprudencia(url_busca)
+        pagina_jurisprudencia = self._obtem_pag_jurisprudencia(url_busca)
+        # import pdb; pdb.set_trace();
+        self._exatrai_dados_tabela(pagina_jurisprudencia)
 
-
-    
 
     def _obtem_pag_jurisprudencia( self, url_busca):
+        """Faz requisição ao TCE-SP e retorna resultado"""
         try:
-            r = requests.get(url_busca)
-            resposta = r.status_code
-            print(r.text)
-            # import pdb; pdb.set_trace();
+            response = requests.get(url_busca)
+            response.raise_for_status()
         except Exception as e:
             print(f'Erro ao requisitar Jurisprudencia - Erro: {str(e)}')
-
+        return response.text
+    
+    def _exatrai_dados_tabela(self, pagina_jurisprudencia):
+        # soup = BeautifulSoup(pagina_jurisprudencia, 'html.parse')
+        soup = BeautifulSoup(pagina_jurisprudencia, 'html.parser')
+        rows = soup.select("table.table-docs tbody tr")
+        print(rows)
