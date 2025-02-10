38fc0a41604833c2d2b961899bfce5d9f5413331
Thiago <thiagoifnmgsi2015@gmail.com>
2025-02-10 17:34:25 -0300

    Cria método que salva arquivo em JSON

--- src/crawler.py
+++ src/crawler.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 import re
+import json
 
 class BuscadorTceSp:
 
@@ -14,8 +15,9 @@ class BuscadorTceSp:
     def baixa_docs(self):
         url_busca = f'{self.URL_BASE}?txtTdPalvs={self.nome_busca.replace(' ', '+')}&txtExp=&txtQqUma=&txtNenhPalvs=&txtNumIni=&txtNumFim=&tipoBuscaTxt=Documento&_tipoBuscaTxt=on&quantTrechos=1&processo=&exercicio=&dataAutuacaoInicio=&dataAutuacaoFim=&dataPubInicio=&dataPubFim=&_relator=1&_auditor=1&_materia=1&_tipoDocumento=1&acao=Executa'
         pagina_jurisprudencia = self._obtem_pag_jurisprudencia(url_busca)
-        self._exatrai_dados_tabela(pagina_jurisprudencia)
-        total_paginas = self._obtem_total_pag(pagina_jurisprudencia)
+        documentos = self._exatrai_dados_tabela(pagina_jurisprudencia)
+        self._obtem_total_pag(pagina_jurisprudencia)
+        self._salva_em_json(documentos, 'dados_jurisprudencia.json')
 
 
     def _obtem_pag_jurisprudencia( self, url_busca):
@@ -36,17 +38,16 @@ class BuscadorTceSp:
         for row in rows:
             cols = row.find_all('td')
             if len(cols) == 8:
-                # import pdb; pdb.set_trace();
                 doc = {
                     "Doc" : cols[0].get_text(strip=True),
                     # "N_processo" : cols[1].get_text(strip=True).split()[0], (Estava pegando informações a mais)
                     # "N_processo": re.match(r'^\d{4,}/\d{3}/\d{2}', cols[1].get_text(strip=True)).group(0)
                     # if re.match(r'^\d{4,}/\d{3}/\d+', cols[1].get_text(strip=True))
                     # else cols[1].get_text(strip=True),
-                    "N_processo" : self._extrair_numero_processo(cols[1].get_text(strip=True)),
+                    "N° processo" : self._extrair_numero_processo(cols[1].get_text(strip=True)),
                     "Data_Atuação" : cols[2].get_text(strip=True),
-                    "Partes" : [parte.strip() for parte in cols[3].get_text(strip=True).split(';')],
-                    "Materia" : cols[4].get_text(strip=True),
+                    "Partes" : [cols[3].get_text(strip=True) + "; " + cols[4].get_text(strip=True)],
+                    "Materia" : cols[5].get_text(strip=True),
                     "URL" : cols[0].find('a')['href'] if cols[0].find('a') else ""
                 }
                 documentos.append(doc)
@@ -54,16 +55,18 @@ class BuscadorTceSp:
     
     def _extrair_numero_processo(self, texto):
         """Extrai número do processo do campo correspondente"""
-        match = re.match(r'^\d{4,}/\d{3}/\d{2}', texto)
+        match = re.match(r'^\d{1,}/\d{3,}/\d{2}', texto)
         return match.group(0) if match else texto
-
+    
     def _obtem_total_pag(self, pagina_jurisprudencia):
-        """Retorna o total de páginas da páginação"""
+        """Retorna o total de páginas da paginação"""
         soup = BeautifulSoup(pagina_jurisprudencia, 'html.parser')
         paginas = soup.find_all('a', class_='page-link')
-        # numeros_paginas = [int(pagina.get_text()) for pagina in paginas if pagina.get_text(strip=True).isdigit()]
-        # total_paginas = max(numeros_paginas, default=0)
-        total_paginas = max([int(pagina.get_text()) for pagina in paginas if pagina.get_text(strip=True).isdigit()])
+        total_paginas = max([int(pagina.get_text()) for pagina in paginas if pagina.get_text(strip=True).isdigit()], default=1)
         return total_paginas
-    
 
+    def _salva_em_json(self, dados, nome_arquivo):
+        """Salva os dados extraídos em um arquivo JSON"""
+        with open(nome_arquivo, 'w', encoding='utf-8') as f:
+            json.dump(dados, f, ensure_ascii=False, indent=4)
+        print(f"Dados salvos em {nome_arquivo}")
